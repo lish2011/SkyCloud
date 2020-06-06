4bdb49871308239cdd5db7c8116612dc846ee17e
lish2011 <ls637@qq.com>
2020-06-06 15:23:07 +0800

    添加多种控件demo

---
+++ WpfDemo-master/WPFDemo/Model/OrgModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace WPFDemo.Model
+{
+    public class OrgModel
+    {
+        private bool _isGrouping;
+        public bool IsGrouping { get { return Children != null && Children.Count > 0; }  set {; } }
+        public ObservableCollection<OrgModel> Children { get; set; }
+        public string DisplayName { get; set; }
+        public string SurName { get; set; }
+        public string Name { get; set; }
+        public string Info { get; set; }
+        public int Count { get; set; }
+    }
+}
